7474c7bc61e7b8f0a765be752d183b2cfd793cc5
Gabriel Flores Guimaraes <163225847+gabrielfloresguimaraes@users.noreply.github.com>
2024-04-17 22:34:32 -0300

    Primeira Classe

---
+++ mod5/PrimeiraClasse.java
@@ -0,0 +1,11 @@
+package br.com.bielf;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World");
+
+	}
+
+}
