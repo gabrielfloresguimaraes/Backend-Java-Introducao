3cb7c3b8f725d33d79823704b6cc1d7b8a85b075
Gabriel Flores Guimaraes <163225847+gabrielfloresguimaraes@users.noreply.github.com>
2024-05-12 22:13:07 -0300

    Padrões de Projeto - Abstract Factory Method

---
+++ mod15/ContratosFactory.java
@@ -0,0 +1,12 @@
+package br.com.bielf;
+
+public class ContratosFactory extends Factory {
+    @Override
+    Car retrieveCar(String requestedGrade) {
+        if ("A".equals(requestedGrade)) {
+            return new CorolaCar(100, "cheio", "azul");
+        } else {
+            return null;
+        }
+    }
+}
---
+++ mod15/CorolaCar.java
@@ -0,0 +1,8 @@
+package br.com.bielf;
+
+public class CorolaCar extends Car{
+
+    public CorolaCar(int horsePower, String fuelSource, String color) {
+        super(horsePower, fuelSource, color);
+    }
+}
