cf8dbdb4c64bf59db2681d9325fbed4c0b33fc6f
Gabriel Flores Guimaraes <163225847+gabrielfloresguimaraes@users.noreply.github.com>
2024-06-12 23:17:33 -0300

    AppReflections

---
+++ mod19/AppReflections/Produto.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Mod19;
+
+/**
+ *
+ * @author bielf
+ */
+public class Produto {
+
+    private Long codigo;
+
+    private String descricao;
+
+    private Double valor;
+
+    public Produto() {
+
+    }
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+}
